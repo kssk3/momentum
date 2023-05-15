1af039ae4bcdf3d01cc1094d038d14b3ce22e1e4
kssk3 <122779182+kssk3@users.noreply.github.com>
2023-05-15 18:43:04 +0900

    2023.5.15 update

--- js/weather.js
+++ js/weather.js
@@ -4,9 +4,14 @@ const API_KEY = "1938138c975033f862c141a1719ce306";
 
 function onGeoOk(position) {
     const lat = position.coords.latitude;
-    const log = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}lon=${log}&appid=${API_KEY}`;
-    fetch(url);
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}lon=${lon}&appid=${API_KEY}`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((date) => {
+            city.innerText = date.name;
+            weather.innerText = `${data.weather[0].main} / ${date.main.temp}`;
+        });
 }
 function onGeoError() {
     alert("Can't find you. No weahter for you. ");
